e84a2a05fd2b8c60e2003436b64add1d988fa10b
vcorneau <vcorneau@gmail.com>
2023-03-30 17:09:15 -0400

    Correct function to return computer choice

--- rps-script.js
+++ rps-script.js
@@ -1,13 +1,13 @@
-function getComputerChoice() {
-    return Math.random()*2;
+function getComputerChoice() { //function returns a random integer between 0 and 2
+    return Math.floor(Math.random()*3);
 }
 
-let computerChoice = getComputerChoice();
+let result = getComputerChoice(); //Computer choice is equal to the result of getComputerChoice()
 
-if (computerChoice = 0)  {
+if (result === 0)  {
     console.log("Rock");
-} else if (computerChoice = 1) {
+} else if (result === 1) {
     console.log("Paper");
-} else if (computerChoice = 2) {
+} else if (result === 2) {
     console.log("Scisors");
 }
