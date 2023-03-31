828b4fe3dbe39cf3b7d3b637eef3d3efdd4e1ad9
vcorneau <vcorneau@gmail.com>
2023-03-31 15:42:48 -0400

    Add playRound() function
    Issue: only returns player as winner

--- rps-script.js
+++ rps-script.js
@@ -2,12 +2,50 @@ function getComputerChoice() { //function returns a random integer between 0 and
     return Math.floor(Math.random()*3);
 }
 
-let result = getComputerChoice(); //Computer choice is equal to the result of getComputerChoice()
-
-if (result === 0)  {
+/*if (computerSelection === 0)  {
     console.log("Rock");
-} else if (result === 1) {
+} else if (computerSelection === 1) {
     console.log("Paper");
-} else if (result === 2) {
-    console.log("Scisors");
+} else if (computerSelection === 2) {
+    console.log("Scissors");
+}*/
+
+
+
+function playRound(){
+   
+    let computerSelection = getComputerChoice(); //Computer choice is equal to the result of getComputerChoice()
+    let playerSelection = prompt("Type in Rock, Paper or Scissors:");
+    let lowercaseSelection = playerSelection.toLowerCase();
+    let playerInt;
+
+    parseInt(lowercaseSelection);
+
+    if (lowercaseSelection === "rock") {
+        let playerInt = 0;
+    } 
+    else if (lowercaseSelection === "paper") {
+        let playerInt = 1;
+    }
+    else if (lowercaseSelection === "scissors") {
+        let playerInt = 2;
+    }
+    else {
+        alert("You've entered an incorrect value. Please reload and try again.")
+    }
+
+    let result = ( playerInt - computerSelection );
+    let winner
+
+    if (result === 1 || -2 ) {
+        return winner = "Player";
+    }
+    else if (result === -1 || 2 ) {
+        return winner = "Computer";
+    }
+    else if (result === 0 ) {
+        return winner = "Draw";
+    }
 }
+
+console.log(playRound());
