127a23f235add310aadf9ad5c6f118a5409a29d9
vcorneau <vcorneau@gmail.com>
2023-04-01 11:26:44 -0400

    Execute one round of RPS
    Issue: if input other than rock, paper or scissors, program doesn't work.

--- rps-script.js
+++ rps-script.js
@@ -1,5 +1,4 @@
 /*Receive player's input and set it to lowercase*/
-
 function getPlayerChoice() {
     let playerPrompt = prompt("Type in Rock, Paper or Scissors:").toLowerCase();
     let playerInt;
@@ -21,47 +20,55 @@ function getPlayerChoice() {
 }
 
 /*Computer generates a random number between 0 and 2*/
-
 function getComputerChoice() { 
     return Math.floor(Math.random()*3);
 }
 
-let computerSelection = getComputerChoice();
-let playerSelection = getPlayerChoice();
-
-if (computerSelection === 0)  {
-    console.log("Computer chose rock");
-} else if (computerSelection === 1) {
-    console.log("Computer chose paper");
-} else if (computerSelection === 2) {
-    console.log("Computer chose scissors");
-}
-
-if (playerSelection === 0)  {
-    console.log("Player chose rock");
-} else if (playerSelection === 1) {
-    console.log("Player chose paper");
-} else if (playerSelection === 2) {
-    console.log("Player chose scissors");
-}
-
-
-
+/*Plays one round of the game*/
 function playRound(){
+
+    let computerSelection = getComputerChoice();
+    let playerSelection = getPlayerChoice();
+
+    if (computerSelection === 0)  {
+        console.log("Computer chose rock");
+    } else if (computerSelection === 1) {
+        console.log("Computer chose paper");
+    } else if (computerSelection === 2) {
+        console.log("Computer chose scissors");
+    }
+
+    if (playerSelection === 0)  {
+        console.log("Player chose rock");
+    } else if (playerSelection === 1) {
+        console.log("Player chose paper");
+    } else if (playerSelection === 2) {
+        console.log("Player chose scissors");
+    }
 
     /*Where Rock is 0, Paper is 1 and Scissors is 2, 
     and the player is on the X axis 
     and CPU on the Y axis of the array. */
 
-    let playerStatus = [
-        ['draw','win','lose']
-        ['lose','draw','win']
-        ['win','lose','draw']
+    
+    var winArray = [
+        ["draw", "win", "lose"],
+        ["lose", "draw", "win"],
+        ["win", "lose", "draw"],
     ]
 
-    console.log(playerStatus[playerSelection,computerSelection]);
+    let winner = (winArray[computerSelection][playerSelection]);
+
+    return winner;
+
 }
 
 let firstRound = playRound();
 
-console.log(firstRound);
+if (firstRound === "win")  {
+    console.log("Player wins!");
+} else if (firstRound === "lose") {
+    console.log("Player loses.");
+} else if (firstRound === "draw") {
+    console.log("It's a draw");
+}
