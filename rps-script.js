26c454c483834b7019a2132446d246623da3fb4d
vcorneau <vcorneau@gmail.com>
2023-04-01 10:39:59 -0400

    Add getPlayerChoice and convert to int.

--- rps-script.js
+++ rps-script.js
@@ -1,51 +1,67 @@
-function getComputerChoice() { //function returns a random integer between 0 and 2
+/*Receive player's input and set it to lowercase*/
+
+function getPlayerChoice() {
+    let playerPrompt = prompt("Type in Rock, Paper or Scissors:").toLowerCase();
+    let playerInt;
+
+    if (playerPrompt === "rock") {
+    playerInt = 0;
+    } 
+    else if (playerPrompt === "paper") {
+    playerInt = 1;
+    }
+    else if (playerPrompt === "scissors") {
+    playerInt = 2;
+    }
+    else {
+    playerPrompt = prompt("Reload and try again").toLowerCase();
+    }
+
+    return playerInt;
+}
+
+/*Computer generates a random number between 0 and 2*/
+
+function getComputerChoice() { 
     return Math.floor(Math.random()*3);
 }
 
-/*if (computerSelection === 0)  {
-    console.log("Rock");
+let computerSelection = getComputerChoice();
+let playerSelection = getPlayerChoice();
+
+if (computerSelection === 0)  {
+    console.log("Computer chose rock");
 } else if (computerSelection === 1) {
-    console.log("Paper");
+    console.log("Computer chose paper");
 } else if (computerSelection === 2) {
-    console.log("Scissors");
-}*/
+    console.log("Computer chose scissors");
+}
+
+if (playerSelection === 0)  {
+    console.log("Player chose rock");
+} else if (playerSelection === 1) {
+    console.log("Player chose paper");
+} else if (playerSelection === 2) {
+    console.log("Player chose scissors");
+}
 
 
 
 function playRound(){
-   
-    let computerSelection = getComputerChoice(); //Computer choice is equal to the result of getComputerChoice()
-    let playerSelection = prompt("Type in Rock, Paper or Scissors:");
-    let lowercaseSelection = playerSelection.toLowerCase();
-    let playerInt;
 
-    parseInt(lowercaseSelection);
+    /*Where Rock is 0, Paper is 1 and Scissors is 2, 
+    and the player is on the X axis 
+    and CPU on the Y axis of the array. */
 
-    if (lowercaseSelection === "rock") {
-        let playerInt = 0;
-    } 
-    else if (lowercaseSelection === "paper") {
-        let playerInt = 1;
-    }
-    else if (lowercaseSelection === "scissors") {
-        let playerInt = 2;
-    }
-    else {
-        alert("You've entered an incorrect value. Please reload and try again.")
-    }
+    let playerStatus = [
+        ['draw','win','lose']
+        ['lose','draw','win']
+        ['win','lose','draw']
+    ]
 
-    let result = ( playerInt - computerSelection );
-    let winner
-
-    if (result === 1 || -2 ) {
-        return winner = "Player";
-    }
-    else if (result === -1 || 2 ) {
-        return winner = "Computer";
-    }
-    else if (result === 0 ) {
-        return winner = "Draw";
-    }
+    console.log(playerStatus[playerSelection,computerSelection]);
 }
 
-console.log(playRound());
+let firstRound = playRound();
+
+console.log(firstRound);
