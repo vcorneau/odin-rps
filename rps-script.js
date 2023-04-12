ce98cd4c755badbd3dc8d5e7f98bdc66b3a03b7d
vcorneau <vcorneau@gmail.com>
2023-04-12 08:39:34 -0400

    Play a game on click and log result to console.

--- rps-script.js
+++ rps-script.js
@@ -6,6 +6,17 @@ let buttonClicked = false;
 let userSelection;
 let playerInt;
 
+/*Where Rock is 0, Paper is 1 and Scissors is 2, 
+and the player is on the X axis 
+and CPU on the Y axis of the array. */
+
+    
+let winArray = [
+    ["draw", "player", "lose"],
+    ["lose", "draw", "win"],
+    ["win", "lose", "draw"],
+]
+
 /*Computer generates a random number between 0 and 2*/
 function getComputerChoice() { 
     return Math.floor(Math.random()*3);
@@ -13,25 +24,46 @@ function getComputerChoice() {
 
 for (let button of buttons) {
     button.addEventListener('click', function handleClick() {
-        console.log('a button was clicked');
         buttonClicked = true;
         
+        let computerSelection = getComputerChoice();
+
+        //Converts button clicked to corresponding integer
         if (buttonClicked = true) {
             userSelection = event.target.id;
             }
-            console.log(userSelection);
 
             if (userSelection == 'rockButton') {
                 playerInt = 0;
+                console.log("Player chose rock");
             }
             else if (userSelection == 'paperButton') {
                 playerInt = 1;
+                console.log("Player chose paper");
             }
             else if (userSelection == 'scissorsButton') {
                 playerInt = 2;
+                console.log("Player chose scissors");
             }
-            console.log(playerInt);
+
+        if (computerSelection === 0)  {
+            console.log("Computer chose rock");
+        } else if (computerSelection === 1) {
+            console.log("Computer chose paper");
+        } else if (computerSelection === 2) {
+            console.log("Computer chose scissors");
         }
-    )
+
+        //Win condition 
+        let winner = (winArray[computerSelection][playerInt]);
+
+        if (winner === "win")  {
+            console.log("Player wins!");
+        } else if (winner === "lose") {
+            console.log("Player loses.");
+        } else if (winner === "draw") {
+            console.log("It's a draw");}
+
+    })
 }
 
