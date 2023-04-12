32d19bab681ecb3be326e1cae29f681160dd3775
vcorneau <vcorneau@gmail.com>
2023-04-12 07:08:24 -0400

    Register button click as button ID

--- rps-script.js
+++ rps-script.js
@@ -1,119 +1,35 @@
-const rockButton = document.getElementById('rockButton');
-const paperButton = document.getElementById('paperButton');
-const scissorsButton = document.getElementById('scissorsButton');
+//Declare constant for the buttons on the page. 
 
-/*Add event listener to buttons*/
+const buttons = document.getElementsByClassName("button");
 
-rockButton.addEventListener('click',console.log('Rock click'));
-paperButton.addEventListener('click',console.log('Paper click'));
-scissorsButton.addEventListener('click',console.log('Scissors click'));
+let buttonClicked = false;
 
+let userSelection;
 
-
-/*Receive player's input and set it to lowercase*/
-function getPlayerChoice() {
-    let playerPrompt = prompt("Type in Rock, Paper or Scissors:").toLowerCase();
-    let playerInt;
-
-    if (playerPrompt === "rock") {
-    playerInt = 0;
-    } 
-    else if (playerPrompt === "paper") {
-    playerInt = 1;
-    }
-    else if (playerPrompt === "scissors") {
-    playerInt = 2;
-    }
-    else {
-    playerPrompt = prompt("Reload and try again").toLowerCase();
-    }
-
-    return playerInt;
-}
-
-/*Computer generates a random number between 0 and 2*/
-function getComputerChoice() { 
-    return Math.floor(Math.random()*3);
-}
-
-/*Plays one round of the game*/
-function playRound(){
-
-    let computerSelection = getComputerChoice();
-    let playerSelection = getComputerChoice(); //Temporarily deactivated to not have to input with every reload.
-
-    if (computerSelection === 0)  {
-        console.log("Computer chose rock");
-    } else if (computerSelection === 1) {
-        console.log("Computer chose paper");
-    } else if (computerSelection === 2) {
-        console.log("Computer chose scissors");
-    }
-
-    if (playerSelection === 0)  {
-        console.log("Player chose rock");
-    } else if (playerSelection === 1) {
-        console.log("Player chose paper");
-    } else if (playerSelection === 2) {
-        console.log("Player chose scissors");
-    }
-
-    /*Where Rock is 0, Paper is 1 and Scissors is 2, 
-    and the player is on the X axis 
-    and CPU on the Y axis of the array. */
-
-    
-    var winArray = [
-        ["draw", "player", "lose"],
-        ["lose", "draw", "win"],
-        ["win", "lose", "draw"],
-    ]
-
-    let winner = (winArray[computerSelection][playerSelection]);
-
-    if (winner === "win")  {
-    console.log("Player wins!");
-    } else if (winner === "lose") {
-    console.log("Player loses.");
-    } else if (winner === "draw") {
-    console.log("It's a draw");}
-    
-    return winner;
-}
-
-
-function game() {
-    let totalRounds = 0;
-    let playerScore = 0;
-    let cpuScore = 0;
-
-    do {
-        let currentRound = playRound();
-        if (currentRound === "win") {
-            playerScore++;
+for (let button of buttons) {
+    button.addEventListener('click', function handleClick() {
+        console.log('a button was clicked');
+        buttonClicked = true;
+        if (buttonClicked = true) {
+            userSelection = event.target.id;
+            }
+            console.log(userSelection);
         }
-        else if (currentRound === "lose") {
-            cpuScore++;
-        }
+    )
+}
 
-        totalRounds++;
-        console.log("Total rounds: " +totalRounds);
-        console.log("Score:"+ " Player " + playerScore + " // "  + "Computer " + cpuScore);
-    }
-    while (totalRounds < 5) 
 
-    if (playerScore > cpuScore) {
-        console.log("That was the last round. Player wins!")
-    }
-    else if (playerScore < cpuScore) {
-        console.log("That was the last round. CPU wins!")
-    }
-    else if (playerScore === cpuScore) {
-        console.log("That was the last round. It's a draw!")
-    }
 
-    }
+/*function buttonPressed(e) {
+    console.log(e.target.id); // Get ID of Clicked Element
 
-console.log(game());
+    return e.target.id;
+}*/
 
+//Create event listener for button clicks
+/*for (let button of buttons) {
+  button.addEventListener("click", buttonPressed); 
+}*/
+
+//Get player selection via button clicks
 
